28dcdb43d1daa896c7851125f6f70359a3ca0eb0
skronheim <sarah.kronheim@mail.utoronto.ca>
2023-04-03 09:59:59 -0400

    Minor tweak to graph labels

--- static/js/app.js
+++ static/js/app.js
@@ -18,7 +18,7 @@ function formatData(data, ID) {
   const selectedSample = data.samples.filter(d => d.id == ID)[0];
 
   // Create a new empty list
-  let formatted = []
+  let formatted = [];
 
   // Iterate through the length of one of the lists and push in data
   for (let x = 0; x < selectedSample.otu_ids.length; x++) {
@@ -31,13 +31,13 @@ function formatData(data, ID) {
   }
 
   // Sort dictionaries by sample values into descending order for bar graph, slices them, and reverses the order for graphing
-  let sorted_bar = [...formatted].sort((a, b) => b.sample_value - a.sample_value).slice(0, 10).reverse()
+  let sorted_bar = [...formatted].sort((a, b) => b.sample_value - a.sample_value).slice(0, 10).reverse();
 
   // Sort dictionaries by OTU ID into ascending order for bubble graph
-  let sorted_bubble = [...formatted].sort((a, b) => a.otu_id_num - b.otu_id_num)
+  let sorted_bubble = [...formatted].sort((a, b) => a.otu_id_num - b.otu_id_num);
 
   // Returns the resulting sorted data
-  return { sorted_bar, sorted_bubble }
+  return { sorted_bar, sorted_bubble };
 }
 
 // Function to initialize the page with default plots, metadata, and dropdown menu
@@ -62,12 +62,12 @@ function init(data) {
       automargin: true
     },
     xaxis: {
-      title: { text: "Number Present in Sample" }
+      title: { text: "Number Found in Sample" }
     },
     yaxis: {
       title: { text: "OTU ID" }
     }
-  }
+  };
 
   // Plots the bar chart in the 'bar' wrapper
   Plotly.newPlot("bar", bar, barLayout);
@@ -97,28 +97,28 @@ function init(data) {
     yaxis: {
       title: { text: "Number Found in Sample" }
     }
-  }
+  };
 
   // Plots the bubble chart
   Plotly.newPlot("bubble", bubble, bubbleLayout);
 
   // Gathers initial metadata
-  let selectedMetaData = data.metadata.filter(dataSet => dataSet.id == 940)
+  let selectedMetaData = data.metadata.filter(dataSet => dataSet.id == 940);
 
   // Writes metadata elements to the html
   var selectTag = d3.select("#sample-metadata");
   var paragraphs = selectTag.selectAll("p").data(selectedMetaData).enter();
   
-  paragraphs.append('p').attr('id', 'id').text(d => `id: ${d.id}`)
-  paragraphs.append('p').attr('id', 'ethnicity').text(d => `ethnicity: ${d.ethnicity}`)
-  paragraphs.append('p').attr('id', 'gender').text(d => `gender: ${d.gender}`)
-  paragraphs.append('p').attr('id', 'age').text(d => `age: ${d.age}`)
-  paragraphs.append('p').attr('id', 'location').text(d => `location: ${d.location}`)
-  paragraphs.append('p').attr('id', 'bbtype').text(d => `bbtype: ${d.bbtype}`)
-  paragraphs.append('p').attr('id', 'wfreq').text(d => `wfreq: ${d.wfreq}`)
+  paragraphs.append('p').attr('id', 'id').text(d => `id: ${d.id}`);
+  paragraphs.append('p').attr('id', 'ethnicity').text(d => `ethnicity: ${d.ethnicity}`);
+  paragraphs.append('p').attr('id', 'gender').text(d => `gender: ${d.gender}`);
+  paragraphs.append('p').attr('id', 'age').text(d => `age: ${d.age}`);
+  paragraphs.append('p').attr('id', 'location').text(d => `location: ${d.location}`);
+  paragraphs.append('p').attr('id', 'bbtype').text(d => `bbtype: ${d.bbtype}`);
+  paragraphs.append('p').attr('id', 'wfreq').text(d => `wfreq: ${d.wfreq}`);
 
   // Sets up the dropdown menu
-  dropdownSetup(data)
+  dropdownSetup(data);
 }
 
 // Sets up the function to call when the dropdown value is changed to change the graphs and metadata
@@ -126,51 +126,50 @@ function optionChanged(selection) {
   // Calls the url again to get the data, and defines the full function to change everything
   d3.json(url).then(function(data) {
   
-  // Get and format data for bar and bubble plots
-  let sorted = formatData(data, selection)
-  
-  // Prepares data for the bar chart
-  let barData = {
-    x: sorted.sorted_bar.map(dataPoint => dataPoint.sample_value),
-    y: sorted.sorted_bar.map(dataPoint => dataPoint.otu_id), 
-    text: sorted.sorted_bar.map(dataPoint => dataPoint.otu_label)
-  };
-
-  let bubbleData = {
-    x: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_id_num),
-    y: sorted.sorted_bubble.map(dataPoint => dataPoint.sample_value),
-    text: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_label),
-    marker: {
-      size: sorted.sorted_bubble.map(dataPoint => dataPoint.sample_value),
-      color: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_id_num)
-    }
-  };
+    // Get and format data for bar and bubble plots
+    let sorted = formatData(data, selection);
     
-  // Restyle both bar plot
-  Plotly.restyle("bar", "x", [barData.x]);
-  Plotly.restyle("bar", "y", [barData.y]);
-  Plotly.restyle("bar", "text", [barData.text]);
+    // Prepares data for the bar chart
+    let barData = {
+      x: sorted.sorted_bar.map(dataPoint => dataPoint.sample_value),
+      y: sorted.sorted_bar.map(dataPoint => dataPoint.otu_id), 
+      text: sorted.sorted_bar.map(dataPoint => dataPoint.otu_label)
+    };
 
-  // Restyle bubble chart
-  Plotly.restyle("bubble", "x", [bubbleData.x]);
-  Plotly.restyle("bubble", "y", [bubbleData.y]);
-  Plotly.restyle("bubble", "text", [bubbleData.text]);
-  Plotly.restyle("bubble", "marker.size", [bubbleData.marker.size]);
-  Plotly.restyle("bubble", "marker.color", [bubbleData.marker.color]);
+    let bubbleData = {
+      x: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_id_num),
+      y: sorted.sorted_bubble.map(dataPoint => dataPoint.sample_value),
+      text: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_label),
+      marker: {
+        size: sorted.sorted_bubble.map(dataPoint => dataPoint.sample_value),
+        color: sorted.sorted_bubble.map(dataPoint => dataPoint.otu_id_num)
+      }
+    };
+      
+    // Restyle both bar plot
+    Plotly.restyle("bar", "x", [barData.x]);
+    Plotly.restyle("bar", "y", [barData.y]);
+    Plotly.restyle("bar", "text", [barData.text]);
 
-  // Gather metadata to show
-  let selectedMetaData = data.metadata.filter(dataSet => dataSet.id == parseInt(selection))[0];
-  console.log(selectedMetaData)
-  // Write metadata to webpage
-  d3.select("#id").text(`id: ${selectedMetaData.id}`);
-  d3.select("#ethnicity").text(`ethnicity: ${selectedMetaData.ethnicity}`);
-  d3.select("#gender").text(`gender: ${selectedMetaData.gender}`);
-  d3.select("#age").text(`age: ${selectedMetaData.age}`);
-  d3.select("#location").text(`location: ${selectedMetaData.location}`);
-  d3.select("#bbtype").text(`bbtype: ${selectedMetaData.bbtype}`);
-  d3.select("#wfreq").text(`wfreq: ${selectedMetaData.wfreq}`);
+    // Restyle bubble chart
+    Plotly.restyle("bubble", "x", [bubbleData.x]);
+    Plotly.restyle("bubble", "y", [bubbleData.y]);
+    Plotly.restyle("bubble", "text", [bubbleData.text]);
+    Plotly.restyle("bubble", "marker.size", [bubbleData.marker.size]);
+    Plotly.restyle("bubble", "marker.color", [bubbleData.marker.color]);
 
+    // Gather metadata to show
+    let selectedMetaData = data.metadata.filter(dataSet => dataSet.id == parseInt(selection))[0];
+
+    // Write metadata to webpage
+    d3.select("#id").text(`id: ${selectedMetaData.id}`);
+    d3.select("#ethnicity").text(`ethnicity: ${selectedMetaData.ethnicity}`);
+    d3.select("#gender").text(`gender: ${selectedMetaData.gender}`);
+    d3.select("#age").text(`age: ${selectedMetaData.age}`);
+    d3.select("#location").text(`location: ${selectedMetaData.location}`);
+    d3.select("#bbtype").text(`bbtype: ${selectedMetaData.bbtype}`);
+    d3.select("#wfreq").text(`wfreq: ${selectedMetaData.wfreq}`);
 })}
 
 // Fetch the data and performs the full function
-d3.json(url).then(init)
+d3.json(url).then(init);
